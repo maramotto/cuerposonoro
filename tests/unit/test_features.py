17c955e160dcdfa7f80b6b7dbef4bf398a075876
nukyma <mara.jounard@gmail.com>
2026-02-13 14:14:46 +0100

    Move file to new folder

--- tests/unit/test_features.py
+++ tests/unit/test_features.py
@@ -14,7 +14,7 @@ import pytest
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../.."))
 
 from vision_processor.features import FeatureExtractor
 
